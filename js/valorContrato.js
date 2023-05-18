a21c099492a3d5d503a53f5f2a27d65fd6dfeaf1
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-05-18 19:17:54 -0300

    Correção do JS

--- js/valorContrato.js
+++ js/valorContrato.js
@@ -1,53 +1,6 @@
-//function valorOper() {
-//  
-//  let campos = document.querySelectorAll('.valor');  // coleta seleciona o campo .valor -> classe
-//  
-//  sum = 0
-//  for (let index = 0; index < campos.length; index++) { // metodo for para percorrer todos os inputs na classe .valor
-//
-//     sum += Number(campos[index].value); //somar todos os campos pelo index 0-15
-//
-//     if (campos === null || campos === "" || isNaN(campos[index])) { // faça algo se o valor focar null, NaN ou uma string vazia
-//      campos[index] = 0
-//      }
-//  }
-//    let valoresC = document.getElementById('td_ValorTotalBoletimOPC').value = `${Number(sum).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`;
-//   console.log(valoresC)
-//
-//}
-//
-//function valorResult() {
-//    let cmpCont = document.getElementById('td_ValorTotalBoletimOPC').value;
-//    let valorContrato = document.getElementById('td_ValorTotalContratoOPC').value;
-//    console.log(valorContrato, typeof valorContrato);
-//
-//    let counter = (cmpCont, valorContrato) => {
-//
-//      if (counter ===isNaN || counter === null || counter === "" || counter === undefined ) {
-//        counter = (cmpCont - valorContrato).toFixed().toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-//      }
-//
-//      console.log("saldo total contratual"+ counter, typeof counter)
-//      return (cmpCont - valorContrato);
-//    }
-//
-//    console.log(cmpCont - valorContrato)
-//    
-//  };
-//
-
-
-//function valorResult(){
-//  let valorResult  = document.getElementById('td_SaldoTotalContratualOPC').value = cmpCont - valorContrato
-//  console.log(valorResult, typeof valorResult)
-//
-//}
-//
-//.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-
-function valorOper() {
+function CalculaContrato() {
   
-  let campos = document.querySelectorAll('#valor');
+  let campos = document.querySelectorAll('.valor_input');
   campos// coleta seleciona o campo .valor -> classe
   
   sum = 0
@@ -61,16 +14,20 @@ function valorOper() {
   }
     let valoresC = document.getElementById('contratos').value = Number(sum).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
     console.log(valoresC)
+    result();
 }
 
 function result() {
-  let valorcontratoReal = document.getElementById('valorContratoReal').value;
-  let contratos = document.getElementById('contratos').value;
-  let valorContrato = document.getElementById('valorCont').value =  valorcontratoReal.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+  let valorContrato1 = document.getElementById('valorContratoReal').value;
+  let valorContrato2 = document.getElementById('valorCont').value = valorContrato1;
 
   
-  let formtRestante = document.getElementById('valorResult').value = (contratos - valorContrato).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-  
+  let formtRestante = document.getElementById('valorResult');
+  formtRestante.value = (valorContrato2 - sum).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
   
   console.log(formtRestante);
 }
+
+
+botao = document.querySelector(".botaoCalc");
+botao.onclick = CalculaContrato;
